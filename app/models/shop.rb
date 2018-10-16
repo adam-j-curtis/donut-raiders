dc49c9fcfb34280895809b8b433af1dc03ab6ba6
Ethan <ethan.schwelling@gmail.com>
2018-10-16 14:19:46 -0400

    Another fix to the model (didn't like the association without :donuts available).  Updated tests to separate elements.

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,6 +1,9 @@
 class Shop < ApplicationRecord
-  has_many :donuts
-  has_many :reviews, through :donuts
+
 
   validates :name, :address, :city, :state, presence: true
 end
+
+# To implement after setup steps
+# has_many :donuts
+# has_many :reviews, through :donuts
