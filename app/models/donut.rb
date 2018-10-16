9febd61012952bf6abfd8658a537dee139a13cda
Ethan <ethan.schwelling@gmail.com>
2018-10-16 15:49:43 -0400

    model and model test update

--- app/models/donut.rb
+++ app/models/donut.rb
@@ -1,5 +1,7 @@
 class Donut < ApplicationRecord
   validates :name, presence: true
+
+  belongs_to :category
 end
 
 # To add association to :shop and :reviews.
--- spec/models/donut_spec.rb
+++ spec/models/donut_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Donut, :type => :model do
   it "is not valid without a name" do
-    donut = Donut.new(name: nil, category: "Traditional")
+    donut = Donut.new(name: nil, category_id: 1)
     expect(donut).to_not be_valid
     expect(donut.errors.messages).to eq(:name=>["can't be blank"])
   end
 
   it "is not valid without a category" do
-    donut = Donut.new(name: "Chocolate Glaze", category: nil)
+    donut = Donut.new(name: "Chocolate Glaze", category_id: nil)
     expect(donut).to_not be_valid
     expect(donut.errors.messages).to eq(:category=>["can't be blank"])
   end
