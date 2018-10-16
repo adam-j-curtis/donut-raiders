feb18b1b55ca12546f189b4093df6ac70b145da2
Ethan <ethan.schwelling@gmail.com>
2018-10-16 10:38:46 -0400

    fixed routes.rb, changed shop to shops

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'homes#index'
   devise_for :users
 
-  resources :shop, only: [:index]
+  resources :shops, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
