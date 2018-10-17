4794cdd5bc4c92291e494479e32d07016a9db82b
Ethan <ethan.schwelling@gmail.com>
2018-10-17 15:59:19 -0400

    donut index page and feature test

--- spec/features/user_visits_donut_index_page_spec.rb
+++ spec/features/user_visits_donut_index_page_spec.rb
@@ -3,20 +3,19 @@ require 'rails_helper'
 feature 'user visits donut index page' do
   scenario 'user sees a list of donuts' do
 
-    Shop.create(name: "Dunkin' Donuts", address: "56 Westview Lane", city: "Pittsburgh", state: "PA", url:"www.google.com", phone:"123-456-7890")
+    dunkin = Shop.create(name: "Dunkin' Donuts", address: "56 Westview Lane", city: "Pittsburgh", state: "PA", url:"www.google.com", phone:"123-456-7890")
 
-    Shop.create(name: "Honeydew Donuts", address: "1600 Pennsylvia Ave NW", city: "Washington, DC", state: "DC", url:"www.honeydewrules.org", phone:"111-111-1112")
+    honeydew = Shop.create(name: "Honeydew Donuts", address: "1600 Pennsylvia Ave NW", city: "Washington, DC", state: "DC", url:"www.honeydewrules.org", phone:"111-111-1112")
 
-    Shop.create(name: "Union Square Donuts", address: "55 Bow Street", city: "Somerville", state: "MA", url:"www.donutsarecool.web", phone:"222-222-2222")
+    unionsq = Shop.create(name: "Union Square Donuts", address: "55 Bow Street", city: "Somerville", state: "MA", url:"www.donutsarecool.web", phone:"222-222-2222")
 
-    Category.create(name: "Old-Fashioned")
-    Category.create(name: "Filled")
-    Category.create(name: "Seasonal")
+    oldfashioned = Category.create(name: "Old-Fashioned")
+    filled = Category.create(name: "Filled")
+    seasonal = Category.create(name: "Seasonal")
 
-    donut1 = Donut.create(name: "Sugar-Raised Donut", category_id: 1, shop_id: 3)
-    donut2 = Donut.create(name: "Jelly Donut", category_id: 2, shop_id: 2)
-    donut3 = Donut.create(name: "French Crueller", category_id: 3, shop_id: 1)
-
+    donut1 = Donut.create(name: "Sugar-Raised Donut", category: oldfashioned, shop: unionsq)
+    donut2 = Donut.create(name: "Jelly Donut", category: filled, shop: honeydew)
+    donut3 = Donut.create(name: "French Crueller", category: seasonal, shop: dunkin)
 
     title = "Donut Index"
 
@@ -25,8 +24,8 @@ feature 'user visits donut index page' do
     expect(page).to have_content title
 
     expect(page).to have_content donut1.name
-    expect(page).to have_content donut2.name
-    expect(page).to have_content donut3.name
+    expect(page).to have_content donut2.category.name
+    expect(page).to have_content donut3.shop.name
 
   end
 end
