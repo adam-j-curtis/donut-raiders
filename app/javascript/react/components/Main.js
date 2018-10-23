b1fd0a465778abc58532a6bb8e359a06132785a2
Ethan <ethan.schwelling@gmail.com>
2018-10-23 13:57:48 -0400

    modified Main.js

--- app/javascript/react/components/Main.js
+++ app/javascript/react/components/Main.js
@@ -2,6 +2,7 @@ import App from './app'
 import React from 'react';
 import { Router, browserHistory, Route, IndexRoute } from 'react-router';
 import DonutIndex from '../containers/DonutIndex'
+import DonutShow from '../containers/DonutShow'
 
 
 
@@ -9,6 +10,7 @@ const Main = (props) => {
   return(
     <Router history={browserHistory}>
       <Route path='/donuts' component={DonutIndex}/>
+      <Route path='/donut/:id' component={DonutShow}/>
     </Router>
   )
 }
